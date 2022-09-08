59ef5b8fd742ba86a1a4203ac851a3f05d316ab6
Lívia <liviamarcelasouza@hotmail.com>
2022-09-07 22:11:51 -0300

    mssql

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import * as sql from "mssql";
 
 @Injectable()
 export class AppService {
-  getHello(): string {
+  async getHello(): Promise<string> {
+    await sql.connect("Data Source=DESKTOP-KAQ762C\\SQLEXPRESS;Database=PHF;Persist Security Info=True;User ID=SA;Password=123456;TrustServerCertificate=True;");
+    let x = await sql.query("select top 1 * from [user]");
     return 'Hello World!';
   }
 }
